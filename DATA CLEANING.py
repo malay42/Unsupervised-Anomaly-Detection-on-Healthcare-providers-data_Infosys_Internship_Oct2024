526d771e09a025dcb03f8b3f77f959993cc7b100
Hari-Mallangi <hmallangiBTECH22@ced.alliance.edu.in>
2024-10-27 14:41:42 +0530

    DATA CLEANING.py

--- DATA CLEANING.py
+++ DATA CLEANING.py
@@ -3,6 +3,8 @@
 # focusing on handling missing values, removing duplicates, and preparing the data for analysis.
 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load dataset
 df = pd.read_csv("C:\\Users\\malla\\Downloads\\archive\\Healthcare Providers.csv")
