67f62f0d6c677c3a8655f7998c258f356d83e697
Akhya Verma <147432147+Akhya-Verma@users.noreply.github.com>
2024-11-07 11:35:33 +0530

    Delete dataexploration.py

--- dataexploration.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib as plt
-df=pd.read_csv('Healthcare Providers.csv')
-df
-print(df.head())
-print(df.describe())
-print(df.info())
